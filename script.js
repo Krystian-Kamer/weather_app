2afa13c1551787ed3b1327d22212a7c5a7dfa323
Krystian <texen24@gmail.com>
2023-05-08 21:42:11 +0200

    creadet function getWeather

--- script.js
+++ script.js
@@ -11,5 +11,19 @@ const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=4a1f423c13e0356dad859d34c9de33e0'
 const API_UNITS = '&units=metric'
 
+const getWeather = () => {
+	const city = input.value || 'Lodz'
+	const URL = API_LINK + city + API_KEY + API_UNITS
 
+	axios.get(URL).then(res => {
+		console.log(res.data)
+		const temp = res.data.main.temp
+		const hum = res.data.main.humidity
+		cityName.textContent = res.data.name
+		temperature.textContent = Math.floor(temp) + '°C'
+		humidity.textContent = hum + '%'
 
+	})
+}
+
+getWeather()
