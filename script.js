beec369ac0745d34d606825087a49c62405fd544
Krystian <texen24@gmail.com>
2023-05-08 21:26:21 +0200

    added consts in js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+const API_KEY = '&appid=4a1f423c13e0356dad859d34c9de33e0'
+const API_UNITS = '&units=metric'
+
+
+
